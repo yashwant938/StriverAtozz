ee603e245c188f9d96046bc2d08a8b4c976f5108
yashwant938 <74610508+yashwant938@users.noreply.github.com>
2023-08-28 23:24:28 +0530

    recursion

---
+++ Day14/classess.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+class hello
+{
+public:
+    int age;
+    char name[20];
+
+private:
+    int id;
+     hello()
+    {
+        std::cout<<"this is default constructuor::"<<std::endl;
+    }
+};
+int main()
+{
+      hello as;
+    std::cout<<"enter the name of a stduent"<<std::endl;
+    std::cin>>name;
+    as.name();
+    std::cout<<"enter the age of a student "<<std::endl;
+    std::cin>>age;
+
+
+
+  
+    
+  
+  
+}
